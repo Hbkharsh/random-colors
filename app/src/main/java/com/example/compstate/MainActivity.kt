9a732f39dcc37965635f6563da025a88a32f8699
Harsh Vardhan <devharsh2k4@gmail.com>
2023-10-06 17:12:02 +0530

    initial commit

---
+++ app/src/main/java/com/example/compstate/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.compstate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.compstate.ui.theme.CompStateTheme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            val color = remember {
+                mutableStateOf(Color.Yellow)
+            }
+         Column(Modifier.fillMaxSize()) {
+             ColorBox(
+
+                 Modifier
+                     .fillMaxSize()
+                     .weight(1f)
+             ){
+                 color.value = it
+             }
+             Box(modifier = Modifier
+                 .fillMaxSize()
+                 .weight(1f)
+                 .background(color.value))
+         }
+        }
+    }
+}
+
+
+@Composable
+fun ColorBox(
+    modifier : Modifier = Modifier,
+    updateColor: (Color)-> Unit){
+
+
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            )
+
+        })
+}
+
